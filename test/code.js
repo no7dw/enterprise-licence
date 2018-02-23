c79344a0e76d894677b199654ab622abf11029f1
no7dw <no7david@gmail.com>
2018-02-23 18:50:41 +0800

    add type test

--- test/code.js
+++ test/code.js
@@ -6,5 +6,9 @@ describe('validate', function(){
         let code = '99540604560888933Q'
         validate(code).valid.should.be.equal(true)
     })
+    it('should valid type', function(){
+        let code = '91440101321036511E'
+        validate(code).type.should.be.equal('企业')
+    })
 })
 
